64687cf3063bbdea2cab436b52f99652a1ddf651
Martha Tran <siutsz@uw.edu>
2019-11-05 14:10:00 -0800

    added project

---
+++ calculator.js
@@ -0,0 +1,27 @@
+let w='1';
+let x=1;
+let y=2;
+let z=3;
+
+if ((x+y)==z) {
+  console.log(z);
+}
+
+if (x===w) {
+  console.log (true);
+}
+else {
+  console.log (false);
+}
+
+if (y <= (z-x)) {
+  console.log('it is the same!');
+}
+
+if (z>y && y>x) {
+  console.log('these are in decending order');
+}
+
+if (y-z != x) {
+  console.log('true');
+}
