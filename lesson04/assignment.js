9519cd661d2144bc624ec2d8d0a1d01a9359dfbe
Msiutsz <51454510+Msiutsz@users.noreply.github.com>
2020-01-07 15:07:45 -0800

    Added lesson 4

---
+++ lesson04/assignment.js
@@ -0,0 +1,12 @@
+function numberSquared() {
+  let input = prompt('Enter a number');
+  result = input*input;
+  alert(result);
+}
+function numberMultiply() {
+  let input1 = prompt('Enter a number');
+  let input2 = prompt('Enter another number');
+  result = input1*input2;
+  alert(result);
+  
+}
